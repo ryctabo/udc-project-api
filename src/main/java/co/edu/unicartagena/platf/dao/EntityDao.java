74f4afcc4c8dd09379f36461bc3035845dda9ada
Gustavo Pacheco <ryctabo@gmail.com>
2016-03-24 03:10:27 -0500

    Create default dao and user dao
    Modify the main methods of the default entity dao
    - findAll, find, save, delete
    
    Create user dao for get data from username and email, the methods
    is following:
    - find by username
    - find by email
    - find by username or email, this method can give you any two args

--- src/main/java/co/edu/unicartagena/platf/dao/EntityDao.java
+++ src/main/java/co/edu/unicartagena/platf/dao/EntityDao.java
@@ -17,8 +17,12 @@ package co.edu.unicartagena.platf.dao;
 
 import co.edu.unicartagena.platf.entity.IEntity;
 import java.util.List;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaQuery;
 
 /**
  * @param <T> Entity Class
@@ -27,7 +31,7 @@ import javax.persistence.Persistence;
  * @author Gustavo Pacheco <ryctabo@gmail.com>
  * @version 0.1
  */
-public class EntityDAO<T extends IEntity, I> implements DataAccessObject<T, I> {
+public class EntityDao<T extends IEntity, I> implements DataAccessObject<T, I> {
     
     /**
      * Entity class.
@@ -45,32 +49,92 @@ public class EntityDAO<T extends IEntity, I> implements DataAccessObject<T, I> {
     private static final EntityManagerFactory FACTORY = 
             Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
 
-    public EntityDAO(Class<T> entityClass) {
+    public EntityDao(Class<T> entityClass) {
         this.entityClass = entityClass;
+    }
+    
+    /**
+     * get entity manager factory
+     * @return entity manager factory
+     */
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return FACTORY;
+    }
+    
+    /**
+     * This method create a entity manager of a factory
+     * @return entity manager
+     */
+    protected EntityManager getEntityManager() {
+        return FACTORY.createEntityManager();
     }
     
     @Override
     public List<T> findAll() {
-        throw new UnsupportedOperationException("Not supported yet.");
-        //To change body of generated methods, choose Tools | Templates.
+        EntityManager entityManager = getEntityManager();
+        try {
+            CriteriaQuery criteriaQuery = entityManager.getCriteriaBuilder().createQuery();
+            criteriaQuery.select(criteriaQuery.from(this.entityClass));
+            
+            TypedQuery<T> typedQuery = entityManager.createQuery(criteriaQuery);
+            return typedQuery.getResultList();
+        } finally {
+            if (entityManager != null)
+                entityManager.close();
+        }
     }
 
     @Override
     public T find(I id) {
-        throw new UnsupportedOperationException("Not supported yet.");
-        //To change body of generated methods, choose Tools | Templates.
+        if (id == null)
+            return null;
+        
+        EntityManager entityManager = getEntityManager();
+        try {
+            return entityManager.find(this.entityClass, id);
+        } finally {
+            if (entityManager != null)
+                entityManager.close();
+        }
     }
 
     @Override
     public T save(T entity) {
-        throw new UnsupportedOperationException("Not supported yet.");
-        //To change body of generated methods, choose Tools | Templates.
+        EntityManager entityManager = getEntityManager();
+        try {
+            EntityTransaction transaction = entityManager.getTransaction();
+            transaction.begin();
+            T entityCreated = entityManager.merge(entity);
+            transaction.commit();
+            return entityCreated;
+        } finally {
+            if (entityManager != null)
+                entityManager.close();
+        }
     }
 
     @Override
     public void delete(I id) {
-        throw new UnsupportedOperationException("Not supported yet.");
-        //To change body of generated methods, choose Tools | Templates.
+        if (id == null)
+            return ;
+        
+        EntityManager entityManager = getEntityManager();
+        try {
+            if (entityManager != null) {
+                EntityTransaction transaction = entityManager.getTransaction();
+                transaction.begin();
+                
+                T entity = this.find(id);
+                
+                if (entity != null)
+                    entityManager.remove(entity);
+                
+                transaction.commit();
+            }
+        } finally {
+            if (entityManager != null)
+                entityManager.close();
+        }
     }
     
 }
---
+++ src/main/java/co/edu/unicartagena/platf/dao/controller/UserDaoController.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2016 Gustavo Pacheco.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package co.edu.unicartagena.platf.dao.controller;
+
+import co.edu.unicartagena.platf.dao.EntityDao;
+import co.edu.unicartagena.platf.dao.exception.NotCreatedEntityManagerException;
+import co.edu.unicartagena.platf.dao.exception.UserNotFoundException;
+import co.edu.unicartagena.platf.entity.User;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author Gustavo Pacheco <ryctabo@gmail.com>
+ * @version 0.1.0
+ */
+public class UserDaoController extends EntityDao<User, Integer> implements UserDao {
+
+    /**
+     * Creates a new instances of <code>UserDaoController</code>.
+     */
+    public UserDaoController() {
+        super(User.class);
+    }
+
+    @Override
+    public User findUserByUsername(String username) throws UserNotFoundException,
+            NotCreatedEntityManagerException {
+        List<Parameter> parameters = Arrays.asList(new Parameter("username", username));
+        String msg = String.format("The user with name %s was not found.", username);
+        return executeNamedQuery(getEntityManager(), "user.findByUsername", parameters, msg);
+    }
+
+    @Override
+    public User findUserByEmail(String email) throws UserNotFoundException,
+            NotCreatedEntityManagerException {
+        List<Parameter> parameters = Arrays.asList(new Parameter("email", email));
+        String errorMessage = String.format("The user with email %s was not found", email);
+        return executeNamedQuery(getEntityManager(), "user.findByEmail", parameters, errorMessage);
+    }
+
+    @Override
+    public User findUserByUsernameOrEmail(String argument) throws UserNotFoundException,
+            NotCreatedEntityManagerException {
+        List<Parameter> params = Arrays.asList(new Parameter("signIn", argument));
+        String errorMeesage = "The username or email is not found";
+        return executeNamedQuery(getEntityManager(), "user.findByUsernameOrEmail", params, errorMeesage);
+    }
+
+    /**
+     * Get first user from named query with given params.
+     * 
+     * @param entityManager entity manager
+     * @param namedQuery named query
+     * @param parameters params for insert into the named query
+     * @param errorMessage the detail message if ocurred a error
+     * @return user
+     * @throws UserNotFoundException if user not found
+     * @throws NotCreatedEntityManagerException if entity user is null
+     */
+    private User executeNamedQuery(EntityManager entityManager, String namedQuery,
+            List<Parameter> parameters, String errorMessage)
+            throws UserNotFoundException, NotCreatedEntityManagerException {
+        if (entityManager != null) {
+            try {
+                TypedQuery<User> typedQuery = entityManager
+                        .createNamedQuery(namedQuery, User.class);
+
+                for (Parameter parameter : parameters) {
+                    typedQuery.setParameter(parameter.key, parameter.value);
+                }
+
+                List<User> users = typedQuery.getResultList();
+                if (users.isEmpty()) {
+                    throw new UserNotFoundException(errorMessage);
+                }
+
+                return users.iterator().next();
+            } finally {
+                entityManager.close();
+            }
+        } else {
+            throw new NotCreatedEntityManagerException("The entity manager is "
+                    + "null, entity manager factory does not create entity manager");
+        }
+    }
+
+    /**
+     * The class <code>Parameter</code> is a argument for insert in SQL, more 
+     * exactly in named query.
+     */
+    private class Parameter {
+
+        String key, value;
+
+        /**
+         * Create a param with key and value.
+         * 
+         * @param key id of value.
+         * @param value object of type string.
+         */
+        public Parameter(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+}
