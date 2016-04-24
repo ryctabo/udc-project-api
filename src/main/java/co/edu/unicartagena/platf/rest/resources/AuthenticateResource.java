688e61b2fb046a53afaa90050d2b32931c556886
Gustavo Pacheco <ryctabo@gmail.com>
2016-04-24 11:18:44 -0500

    fix:Correct login authentication on the resource

--- src/main/java/co/edu/unicartagena/platf/rest/resources/AuthenticateResource.java
+++ src/main/java/co/edu/unicartagena/platf/rest/resources/AuthenticateResource.java
@@ -19,12 +19,12 @@ import co.edu.unicartagena.platf.entity.Person;
 import co.edu.unicartagena.platf.entity.RoleType;
 import co.edu.unicartagena.platf.entity.User;
 import co.edu.unicartagena.platf.exception.TokenNotGeneratedException;
+import co.edu.unicartagena.platf.model.ErrorMessage;
 import co.edu.unicartagena.platf.rest.TokenUtil;
-import co.edu.unicartagena.platf.service.LoginService;
 import co.edu.unicartagena.platf.service.UserService;
 import co.edu.unicartagena.platf.service.UserServiceImpl;
 import co.edu.unicartagena.platf.transfer.TokenTransfer;
-import co.edu.unicartagena.platf.transfer.UserTransfer;
+import co.edu.unicartagena.platf.transfer.LoginTransfer;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -32,7 +32,6 @@ import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -43,34 +42,46 @@ import org.jose4j.lang.JoseException;
 /**
  *
  * @author Gustavo Pacheco <ryctabo@gmail.com>
- * @version 1.0
+ * @version 1.0-SNAPSHOT
  */
-@Path("/")
-@Consumes(MediaType.APPLICATION_JSON)
 public class AuthenticateResource {
-    
-    LoginService service = UserServiceImpl.getInstance();
-    
+
+    UserService service = UserServiceImpl.getInstance();
+
     private static final Logger LOG = Logger.getLogger(AuthenticateResource.class.getName());
-    
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String test() {
         return "Authenticate please!";
     }
-    
+
     @POST
-    public Response authenticate(UserTransfer userTransfer) {
-        String usernameOrEmail = userTransfer.getUsernameOrEmail();
-        String password = userTransfer.getPassword();
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response authenticate(LoginTransfer loginTransfer) {
+        if (loginTransfer == null) {
+            ErrorMessage em = new ErrorMessage(
+                    Response.Status.BAD_REQUEST.getStatusCode(),
+                    "Information for login is required.");
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(em)
+                    .build();
+        }
+
+        String usernameOrEmail = loginTransfer.getUsername();
+        String password = loginTransfer.getPassword();
+
         if (service.login(usernameOrEmail, password)) {
-            User user = ((UserService) service).findByUsernameOrEmail(usernameOrEmail);
+            User user = service.findByUsernameOrEmail(usernameOrEmail);
             String token;
+
             try {
                 TokenUtil.UserInfo userInfo = new TokenUtil.UserInfo();
                 userInfo.setId(user.getId());
                 userInfo.setUsername(user.getUsername());
                 userInfo.setEmail(user.getEmail());
+
                 if (user instanceof Person) {
                     userInfo.setName(((Person) user).toString());
                 } else if (user.getRoles().contains(RoleType.ADMINISTRATOR)) {
@@ -78,19 +89,30 @@ public class AuthenticateResource {
                 } else {
                     userInfo.setName("Not define.");
                 }
+
                 token = TokenUtil.generateToken(userInfo);
             } catch (JoseException | TokenNotGeneratedException ex) {
                 LOG.log(Level.SEVERE, null, ex);
-                throw new WebApplicationException(ex.getMessage(), ex);
+                ErrorMessage em = new ErrorMessage(
+                        Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(),
+                        ex.getMessage());
+                Response response = Response
+                        .status(Response.Status.INTERNAL_SERVER_ERROR)
+                        .entity(em)
+                        .build();
+                throw new WebApplicationException(response);
             }
             return Response
-                    .ok(new TokenTransfer(token), MediaType.APPLICATION_JSON)
+                    .ok(new TokenTransfer(token))
                     .build();
         } else {
-            return Response
-                    .ok("Username or password is not valid.", MediaType.TEXT_PLAIN)
+            ErrorMessage em = new ErrorMessage(
+                    Response.Status.UNAUTHORIZED.getStatusCode(),
+                    "Username or password is not valid.");
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity(em)
                     .build();
         }
     }
-    
+
 }
