28cc4f52db92a5eb5bd0704ce10c1a5fc0aaca1e
Gustavo Pacheco <ryctabo@gmail.com>
2016-04-24 11:38:41 -0500

    Implement user resource
    Implements resource of users with next methods.
    
    GET	/users get all resource
    GET	/users/{username} get user by username
    POST	/users add new user
    PUT	/users/{userId} modify user by user id
    DELETE	/users/{userId} delete user by user id

--- src/main/java/co/edu/unicartagena/platf/rest/resources/UserResource.java
+++ src/main/java/co/edu/unicartagena/platf/rest/resources/UserResource.java
@@ -15,10 +15,26 @@
  */
 package co.edu.unicartagena.platf.rest.resources;
 
+import co.edu.unicartagena.platf.entity.Person;
+import co.edu.unicartagena.platf.entity.User;
+import co.edu.unicartagena.platf.service.UserService;
+import co.edu.unicartagena.platf.service.UserServiceImpl;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 /**
  *
@@ -26,12 +42,54 @@ import javax.ws.rs.core.MediaType;
  * @version 1.0-SNAPSHOT
  */
 @Path("users")
+@Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
     
+    UserService service = UserServiceImpl.getInstance();
+    
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String test() {
-        return "Hello Wolrd!";
+    public List<Person> getAllUsers() {
+        final List<Person> people = new ArrayList<>();
+        for (User user : service.getAll()) {
+            if (user instanceof Person) {
+                people.add((Person) user);
+            }
+        }
+        return people;
+    }
+    
+    @POST
+    public Response addUser(Person userPerson, @Context UriInfo uriInfo) {
+        Person newUserPerson = (Person) service.add(userPerson);
+        String username = newUserPerson.getUsername();
+        URI uri = uriInfo.getAbsolutePathBuilder().path(username).build();
+        return Response.created(uri)
+                .entity(newUserPerson)
+                .build();
+    }
+    
+    @PUT
+    @Path("{userId}")
+    public Person updateUser(@PathParam("userId") Integer userId, Person person) {
+        return (Person) service.update(userId, person);
+    }
+    
+    @DELETE
+    @Path("{userId}")
+    public User deleteUser(@PathParam("userId") Integer userId) {
+        return service.remove(userId);
+    }
+    
+    @GET
+    @Path("{username}")
+    public Response getUserByUsername(@PathParam("username") String username) {
+        User user = service.findByUsernameOrEmail(username);
+        if (user instanceof Person) {
+            Person person = (Person) user;
+            return Response.ok(person).build();
+        }
+        return Response.ok(user).build();
     }
     
     @Path("login")
